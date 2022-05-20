a8ecdf978252f050ef1cd9d825e8dc52ddec75c5
ZFX Laptop <ZFX Laptop@DESKTOP-7QUUBVV.mshome.net>
2022-05-20 12:20:36 -0500

    Archivos necesarios para el proyecto

---
+++ src/taller1_repo_xp_1/Main.java
@@ -0,0 +1,13 @@
+package taller1_repo_xp_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi primer proyecto JAVA en GitHub");
+	//	System.out.println("Mi segundo comentario");
+		
+	}
+
+}
